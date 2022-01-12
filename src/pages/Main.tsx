f4f6080b63a5a43a496d253b56ff7732856dca9b
Yren R <yren.rivero@gmail.com>
2022-01-12 22:10:34 +0100

    feat: Create a simple view for show the analo clock

---
+++ src/pages/Main.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import AnaloClock from "../components/AnaloClock/AnaloClock";
+import "./mainStyle.css";
+
+export default function Main() {
+  return (
+    <div className="main-container">
+      <AnaloClock />
+    </div>
+  );
+}
